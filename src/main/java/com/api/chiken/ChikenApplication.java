cf2b05e89b75d95ba0983a19d00ea6e6f9208e40
Ismael P. Santana <ismakenps@gmail.com>
2023-03-19 19:09:11 -0600

    Initial commit

---
+++ src/main/java/com/api/chiken/ChikenApplication.java
@@ -0,0 +1,13 @@
+package com.api.chiken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChikenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChikenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/chiken/ChikenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.chiken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChikenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
